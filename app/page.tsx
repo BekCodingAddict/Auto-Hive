3bfdc37ffbc9796a61ce5ac8e11fb5a1a42a649d
unknown <experte001@gmail.com>
2025-03-15 22:42:54 +0900

    Car Cart UI Implementations

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCart, CustomFilter, Hero, SearchBar } from "@/components";
 import { fetchCars } from "@/utils";
 import Image from "next/image";
 
 export default async function Home() {
   const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -22,6 +23,21 @@ export default async function Home() {
             <CustomFilter title="fuel" />
             <CustomFilter title="year" />
           </div>
+
+          {!isDataEmpty ? (
+            <section>
+              <div className="home__cars-wrappper">
+                {allCars?.map((car) => (
+                  <CarCart car={car} />
+                ))}
+              </div>
+            </section>
+          ) : (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )}
         </div>
       </div>
     </main>
